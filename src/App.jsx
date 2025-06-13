c6b5f0b1a64f285039c8553e8b670639fe6a35da
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-06-13 09:23:23 +0800

    feat: added navbar footer and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import '../src/assets/styles/App.css'
-
+import Navbar from './components/Navbar.jsx'
+import Footer from './components/Footer.jsx'
+import Home from './components/Home.jsx'
 function App() {
 
   return (
     <>
-      <p>Test</p>
+      <Navbar />
+      <Home />
+      <Footer />
     </>
   )
 }
