cddb716aba0a41c67a632d128aae0b3dd764b32a
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-06-13 10:15:06 +0800

    feat: added sections for main

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
-import Home from './components/Home.jsx'
+import Home from './components/Main.jsx'
 function App() {
 
   return (
     <>
       <Navbar />
+      <Home/>
     </>
   )
 }
