87d874425b8f8d2cf1a2429ce787c8a59b7ef66b
oliverkeanpalgue <106715785+oliverkeanpalgue@users.noreply.github.com>
2025-06-13 10:11:51 +0800

    feat: navbar design

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,3 @@
-import '../src/assets/styles/App.css'
 import Navbar from './components/Navbar.jsx'
 import Footer from './components/Footer.jsx'
 import Home from './components/Home.jsx'
@@ -7,8 +6,6 @@ function App() {
   return (
     <>
       <Navbar />
-      <Home />
-      <Footer />
     </>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,31 @@
 function Navbar() {
   return (
-    <nav>
-      <h1>My Portfolio</h1>
+    <nav className="w-full flex justify-between items-center px-16 py-8 border-b border-gray-200 bg-white shadow-sm">
+      <a href="#home" className="text-purple-950 text-3xl font-bold">
+        Oliver Kean Palgue
+      </a>
+      <ul className="flex gap-8">
+        <li>
+          <a href="#home" className="text-gray-700 hover:text-purple-900 font-medium transition-colors">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="#about" className="text-gray-700 hover:text-purple-900 font-medium transition-colors">
+            About
+          </a>
+        </li>
+        <li>
+          <a href="#projects" className="text-gray-700 hover:text-purple-900 font-medium transition-colors">
+            Projects
+          </a>
+        </li>
+        <li>
+          <a href="#contact" className="text-gray-700 hover:text-purple-900 font-medium transition-colors">
+            Contact
+          </a>
+        </li>
+      </ul>
     </nav>
   );
 }
