6288313338f15fcf1db2b83a40406fefb1e65083
oliverkeanpalgue <oliverpalgue@gmail.com>
2025-06-27 09:04:30 +0800

    refactor: main section and navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,6 @@
 function Navbar() {
   return (
-    <nav className="w-full flex justify-between items-center px-16 py-8 border-b border-gray-200 bg-white shadow-sm">
-      <a href="#home" className="text-purple-950 text-3xl font-bold">
-        Oliver Kean Palgue
-      </a>
+    <nav className="w-full flex justify-center items-center px-16 py-8 border-b border-gray-200 bg-white shadow-sm">
       <ul className="flex gap-8">
         <li>
           <a href="#home" className="text-gray-700 hover:text-purple-900 font-medium transition-colors">
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -1,15 +1,23 @@
 function Home() {
-    return (
-    <section id="home" className="w-full max-w-4xl p-8 bg-white shadow-md rounded-lg mt-8">
-          <h1 className="text-4xl font-bold text-purple-950 mb-4">Welcome to My Portfolio</h1>
-          <p className="text-gray-700 mb-6">
-            Hi, I'm Oliver Kean Palgue, a passionate web developer with a love for creating beautiful and functional websites.
-          </p>
-          <a href="#projects" className="text-purple-950 hover:text-purple-700 font-semibold transition-colors">
+  return (
+    <section id="home" className="w-full p-10">
+      <div className="flex justify-between">
+        <div>
+          <h1 className="text-4xl mb-8 text-purple-900 font-bold">Oliver Kean Palgue</h1>
+          <a
+            href="#projects"
+            className="bg-purple-950 hover:bg-purple-900 text-white p-2 rounded-full font-medium transition-colors"
+          >
             View My Projects
           </a>
-        </section>
-    );
+        </div>
+
+        <div>
+            {/* paragrpah */}
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Home;
