ad2e09c0e06552db8c15e48a2943c34b616eb79f
oliverkeanpalgue <oliverpalgue@gmail.com>
2025-06-29 22:09:21 +0800

    feat: added list for experiences

--- src/sections/Experience/Experience.jsx
+++ src/sections/Experience/Experience.jsx
@@ -1,21 +1,33 @@
+import EXPERIENCE from "../../constants/experience";
 export default function Experience() {
   return (
     <section id="#experiences" className="w-full p-18 shadow-md">
       <div className="flex justify-between gap-2">
-        <ol class="relative border-s border-gray-200 dark:border-gray-700">
-          <li class="mb-10 ms-6">
-            <span class="absolute flex items-center justify-center w-6 h-6 bg-blue-100 rounded-full -start-3 ring-8 ring-white">
-              o
-            </span>
-            <p class="text-gray-500 text-sm">April 2025 - June 2025</p>
-            <h3 class="flex items-center mb-1 text-lg font-semibold text-gray-900">
-              City Government of Baguio - Smart City Command and Control
-            </h3>
-            <p class="mb-4 text-base font-normal text-gray-600">
-              Developed a real-time traffic monitoring system with Laravel and Vue.js, using third-party APIs and Laravel Reverb for live updates and a seamless UI. Created technical documentation to support scalability and team collaboration.
-            </p>
-          </li>
-
+        <ol className="relative border-s border-gray-600">
+          {EXPERIENCE.map(
+            ({ company, date, address, role, description }, index) => (
+              <li className="mb-10 ms-6" key={index}>
+                <span className="absolute flex items-center justify-center w-6 h-6 bg-purple-800 rounded-full -start-3 ring-8 ring-white"></span>
+                <div className="border border-gray-400 p-2 rounded-md">
+                  <p className="text-gray-500 text-sm">{date}</p>
+                  <div className="flex justify-between">
+                    <h3 className="text-xl font-bold text-purple-800">
+                      {company}
+                    </h3>
+                    <span className="text-sm text-gray-500 font-normal pr-2">
+                      {address}
+                    </span>
+                  </div>
+                  <p className="mb-1 text-gray-700 text-lg font-semibold">
+                    {role}
+                  </p>
+                  <p className="mb-4 text-base text-md font-normal text-gray-600">
+                    {description}
+                  </p>
+                </div>
+              </li>
+            )
+          )}
         </ol>
       </div>
     </section>
