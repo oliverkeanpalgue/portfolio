82e3a09e06468ec34e16ece2ff267976f555d704
oliverkeanpalgue <oliverpalgue@gmail.com>
2025-06-29 11:23:33 +0800

    feat: added constants for config site

---
+++ src/constants/constants.js
@@ -0,0 +1,10 @@
+//site configuration
+export const NAME = "Oliver Kean Palgue"
+
+//roles
+export const WEB = "Web Developer"
+export const FRONTEND = "Frontend Developer"
+export const BACKEND = "Backend Developer"
+export const FULL_STACK_ROLE = "Full Stack Developer"
+
+
