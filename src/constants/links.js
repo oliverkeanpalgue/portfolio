26e7d2e04837b3b03490931254a993cdafda0a4f
oliverkeanpalgue <oliverpalgue@gmail.com>
2025-06-29 16:53:26 +0800

    feature:navbar added link components

---
+++ src/constants/links.js
@@ -0,0 +1,24 @@
+const LINK = [
+    {
+        name: 'About',
+        link: ''
+    },
+    {
+        name: 'Experiences',
+        link: '#experiences'
+    },
+    {
+        name: 'Projects',
+        link: '#projects'
+    },
+    {
+        name: 'Skills',
+        link: '#skills'
+    },
+    {
+        name: 'Contact',
+        link: '#contact'
+    }
+]
+
+export default LINK
